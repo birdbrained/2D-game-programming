117cd373831b8f2b3673ee213bb6c97cd063704a
Matthew Kataryniak <mwk9@njit.edu>
2017-09-12 15:58:36 -0400

    Added pq files

---
+++ Fall2017_2Dproject/ds_priority_queue.c
@@ -0,0 +1,21 @@
+#include "ds_priority_queue.h"
+
+PriorityQueue *pq_new(size_t elementSize);
+{
+	return NULL;
+}
+
+void pq_delete(PriorityQueue *pq)
+{
+	return NULL;
+}
+
+void *pq_delete_max(PriorityQueue *pq)
+{
+	return NULL;
+}
+
+void pq_insert(PriorityQueue *pq, void *data, int priority)
+{
+	return NULL;
+}
